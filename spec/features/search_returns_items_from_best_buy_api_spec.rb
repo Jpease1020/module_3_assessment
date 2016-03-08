f14f2124df3ecdc7f8c57f161e6efc2af74f8bfc
Justin Pease <justinpease2@gmail.com>
2016-03-08 11:29:54 -0700

    test complete for first get request

--- spec/features/search_returns_items_from_best_buy_api_spec.rb
+++ spec/features/search_returns_items_from_best_buy_api_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe "search returns items from bestbuy api", type: :feature do
   context "user searches for sennheiser" do
     scenario "it returns items from bestbuy api" do
         visit '/'
+
         VCR.use_cassette("product_search") do
-        # within('search') do
           fill_in("q", with: 'sennheiser')
           click_on "Search"
         end
+
         expect(page).to have_content("Galaxy Audio")
+        expect(page).to have_css("#search-product", :count => 15)
     end
   end
 end
