015695f60ff8782af088297691ba401cf5ba8e99
Justin Pease <justinpease2@gmail.com>
2016-03-08 09:27:19 -0700

    adds test for single item to be returned by internal api call

--- spec/requests/all_item_serialized_spec.rb
+++ spec/requests/all_item_serialized_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "" do
+describe "api/v1/items returns all items serialized" do
   describe "GET /api/v1/items" do
     it "returns all items with name, description, image url but not created at or updataed at" do
       20.times do |i|
@@ -8,12 +8,12 @@ describe "" do
               description: "description#{i}",
                 image_url: "image_url#{i}")
       end
-      #create some objects to have the endpoint return form the database
+
       get "/api/v1/items", {}, { "Accept" => "application/json" }
       expect(response.status).to eq 200
-      #
-      # ?????? = JSON.parse(response.body)
-      # asssert something
+      items = JSON.parse(response.body)
+      assert_equal "item0", items.first['name']
+      assert_equal "item19", items.last['name']
     end
   end
 end
---
+++ spec/requests/single_item_serialized_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "api/v1/item/:id returns an item serialized" do
+  describe "GET /api/v1/items/:id" do
+    it "returns an item with name, description, image url but not created at or updataed at" do
+      20.times do |i|
+        Item.create!(name: "item#{i}",
+              description: "description#{i}",
+                image_url: "image_url#{i}")
+      end
+
+      get "/api/v1/item/1", {}, { "Accept" => "application/json" }
+      expect(response.status).to eq 200
+      items = JSON.parse(response.body)
+      assert_equal "item0", items.first['name']
+    end
+  end
+end
